87f09fa2ecd5d1b1c87aa7fe299e57a5cbc20c5d
chris chen <wasi.chris@gmail.com>
2022-10-19 15:19:17 +0800

    setup eslint with eslint-config-standard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './assets/images/logo.svg';
-
+import React, { ChangeEvent, useState, useEffect } from 'react'
+import logo from './assets/images/logo.svg'
 
 function App() {
+  const [msg, setMsg] = useState('')
+
+  useEffect(() => {
+    console.log('In effect:', msg)
+  }, [msg])
+
+  const handleClick = () => {
+    console.log('first')
+  }
+
+  const handleTxtChanged = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setMsg(e.target.value)
+  }
+
   return (
     <div className="app">
 
@@ -13,12 +27,13 @@ function App() {
 
         <ul className="app-header__nav">
           <li className="app-header__nav-item">Home</li>
+
           <li className="app-header__nav-item app-header__nav-item--active">Store</li>
           <li className="app-header__nav-item">settings</li>
+
         </ul>
 
       </header>
-
 
       <div className="app-body">
 
@@ -26,12 +41,19 @@ function App() {
 
           something put here!!!
 
+          <div>
+            <input type="button" value="click me" onClick={handleClick} />
+            <input type="text" onChange={handleTxtChanged} />
+          </div>
+
+          <p> {msg} </p>
+
         </div>
 
       </div>
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
